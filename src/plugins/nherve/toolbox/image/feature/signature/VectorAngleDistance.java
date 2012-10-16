6f6056c951a4e53b3c872bf59be14fcf52a63279
Nicolas HERVE <n.herve@laposte.net>
2012-10-16 11:40:11 +0200

    VectorAngleDistance + some distance tests

--- src/plugins/nherve/toolbox/image/feature/signature/VectorAngleDistance.java
+++ src/plugins/nherve/toolbox/image/feature/signature/VectorAngleDistance.java
@@ -1,5 +1,4 @@
 /*
- * Copyright 2010, 2011 Institut Pasteur.
  * Copyright 2012 Institut National de l'Audiovisuel.
  * 
  * This file is part of NHerveTools.
@@ -24,38 +23,29 @@ import java.util.Iterator;
 import plugins.nherve.toolbox.image.feature.SignatureDistance;
 
 /**
- * The Class L1Distance.
+ * The Class VectorAngleDistance.
  * 
  * @author Nicolas HERVE - nherve@ina.fr
  */
-public class L1Distance extends SignatureDistance<VectorSignature> {
+public class VectorAngleDistance extends SignatureDistance<VectorSignature> {
 
-	/**
-	 * Instantiates a new l1 distance.
-	 */
-	public L1Distance() {
+	public VectorAngleDistance() {
 		super();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * plugins.nherve.toolbox.image.feature.SignatureDistance#computeDistance
-	 * (plugins.nherve.toolbox.image.feature.Signature,
-	 * plugins.nherve.toolbox.image.feature.Signature)
-	 */
 	@Override
 	public double computeDistance(VectorSignature vs1, VectorSignature vs2) throws SignatureException {
 		if ((vs1 == null) || (vs2 == null)) {
-			throw new SignatureException("Null signature in L1Distance.compute()");
+			throw new SignatureException("Null signature in VectorAngleDistance.compute()");
 		}
 
 		if (vs1.getSize() != vs2.getSize()) {
 			throw new SignatureException("VectorSignature dimensions mismatch");
 		}
 
-		double sum = 0.0;
+		double dist = 0.0;
+		double sqsum1 = 0.0;
+		double sqsum2 = 0.0;
 
 		if (vs1 instanceof SparseVectorSignature && vs2 instanceof SparseVectorSignature) {
 			SparseVectorSignature s1 = (SparseVectorSignature) vs1;
@@ -75,28 +65,38 @@ public class L1Distance extends SignatureDistance<VectorSignature> {
 				idx2 = idx2it.next();
 			}
 
+			double v1 = 0;
+			double v2 = 0;
 			while ((idx1 < Integer.MAX_VALUE) || (idx2 < Integer.MAX_VALUE)) {
 				if (idx1 == idx2) {
-					sum += Math.abs(s1.get(idx1) - s2.get(idx2));
+					v1 = s1.get(idx1);
+					v2 = s2.get(idx2);
+					dist += v1 * v2;
+					sqsum1 += v1 * v1;
+					sqsum2 += v2 * v2;
+
 					if (idx1it.hasNext()) {
 						idx1 = idx1it.next();
 					} else {
 						idx1 = Integer.MAX_VALUE;
 					}
+
 					if (idx2it.hasNext()) {
 						idx2 = idx2it.next();
 					} else {
 						idx2 = Integer.MAX_VALUE;
 					}
 				} else if (idx1 > idx2) {
-					sum += Math.abs(s2.get(idx2));
+					v2 = s2.get(idx2);
+					sqsum2 += v2 * v2;
 					if (idx2it.hasNext()) {
 						idx2 = idx2it.next();
 					} else {
 						idx2 = Integer.MAX_VALUE;
 					}
 				} else {
-					sum += Math.abs(s1.get(idx1));
+					v1 = s1.get(idx1);
+					sqsum1 += v1 * v1;
 					if (idx1it.hasNext()) {
 						idx1 = idx1it.next();
 					} else {
@@ -104,20 +104,42 @@ public class L1Distance extends SignatureDistance<VectorSignature> {
 					}
 				}
 			}
+
 		} else if (vs1 instanceof DenseVectorSignature && vs2 instanceof DenseVectorSignature) {
-			double[] s1 = ((DenseVectorSignature)vs1).getData();
-			double[] s2 = ((DenseVectorSignature)vs2).getData();
-			for (int dim = 0; dim < s1.length; dim++) {
-				sum += Math.abs(s1[dim] - s2[dim]);
+			double[] s1 = ((DenseVectorSignature) vs1).getData();
+			double[] s2 = ((DenseVectorSignature) vs2).getData();
+
+			for (int i = 0; i < s1.length; i++) {
+				dist += s1[i] * s2[i];
+				sqsum1 += s1[i] * s1[i];
+				sqsum2 += s2[i] * s2[i];
 			}
 		} else {
-			for (int dim = 0; dim < vs1.getSize(); dim++) {
-				double e = vs1.get(dim) - vs2.get(dim);
-				sum += Math.abs(e);
+			double v1 = 0;
+			double v2 = 0;
+			for (int i = 0; i < vs1.getSize(); i++) {
+				v1 = vs1.get(i);
+				v2 = vs2.get(i);
+				dist += v1 * v2;
+				sqsum1 += v1 * v1;
+				sqsum2 += v2 * v2;
 			}
 		}
 
-		return sum;
+		double sq = Math.sqrt(sqsum1 * sqsum2);
+
+		if ((sq == 0.) || (dist == 0.)) {
+			dist = 2.;
+		} else {
+			double r = dist / sq;
+			if (r > 1) {
+				dist = 0;
+			} else {
+				dist = Math.acos(r);
+			}
+		}
+
+		return dist;
 	}
 
 }
---
+++ src/plugins/nherve/toolbox/test/TestDistances.java
@@ -0,0 +1,96 @@
+package plugins.nherve.toolbox.test;
+
+import java.util.Random;
+
+import plugins.nherve.toolbox.image.feature.SignatureDistance;
+import plugins.nherve.toolbox.image.feature.signature.DenseVectorSignature;
+import plugins.nherve.toolbox.image.feature.signature.DynamicSparseVectorSignature;
+import plugins.nherve.toolbox.image.feature.signature.L1Distance;
+import plugins.nherve.toolbox.image.feature.signature.L2Distance;
+import plugins.nherve.toolbox.image.feature.signature.SignatureException;
+import plugins.nherve.toolbox.image.feature.signature.SparseVectorSignature;
+import plugins.nherve.toolbox.image.feature.signature.VectorAngleDistance;
+import plugins.nherve.toolbox.image.feature.signature.VectorSignature;
+
+public class TestDistances {
+
+	public static Random rd = new Random(1586412l);
+
+	public static double[] randomData(int dim, double sparsity) {
+		double[] data = new double[dim];
+		for (int d = 0; d < dim; d++) {
+			if (rd.nextDouble() >= sparsity) {
+				data[d] = rd.nextDouble();
+			} else {
+				data[d] = 0.;
+			}
+		}
+		return data;
+	}
+
+	public static void test(int dim, double sparsity, int nb) throws SignatureException {
+		final int sigType = 3;
+		final int distType = 3;
+
+		VectorSignature[][] sigs = new VectorSignature[sigType][nb];
+
+		for (int n = 0; n < nb; n++) {
+			double[] data = randomData(dim, sparsity);
+			DenseVectorSignature dense = new DenseVectorSignature(dim);
+			SparseVectorSignature sparse = new SparseVectorSignature(dim);
+			DynamicSparseVectorSignature dynamic = new DynamicSparseVectorSignature();
+			for (int d = 0; d < dim; d++) {
+				dense.set(d, data[d]);
+				sparse.set(d, data[d]);
+				dynamic.set(d, data[d]);
+			}
+			sigs[0][n] = dense;
+			sigs[1][n] = sparse;
+			sigs[2][n] = dynamic;
+		}
+
+		SignatureDistance<VectorSignature>[] dist = new SignatureDistance[distType];
+		dist[0] = new L1Distance();
+		dist[1] = new L2Distance();
+		dist[2] = new VectorAngleDistance();
+
+		for (int i = 0; i < nb; i++) {
+			for (int j = 0; j < nb; j++) {
+				for (int d = 0; d < distType; d++) {
+					int idx = 0;
+					double previous = 0;
+					for (int m = 0; m < sigType; m++) {
+						for (int n = 0; n < sigType; n++) {
+							double computed = dist[d].computeDistance(sigs[m][i], sigs[n][j]);
+							if (idx > 0) {
+								if (computed != previous) {
+									System.err.println("Error (" + dist[d].getClass().getName() + ") : " + previous + " / " + computed);
+									System.err.println(" - sigs[" + m + "][" + i + "] : " + sigs[m][i]);
+									System.err.println(" - sigs[" + n + "][" + j + "] : " + sigs[n][j]);
+									System.err.println("");
+									System.exit(1);
+								}
+							}
+							idx++;
+							previous = computed;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			for (int dim = 2; dim < 100; dim++) {
+				for (double sparsity = 0; sparsity < 0.9; sparsity += 0.1) {
+					System.out.println("test(" + dim + ", " + sparsity + ", 10);");
+					test(dim, sparsity, 10);
+				}
+			}
+		} catch (SignatureException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
