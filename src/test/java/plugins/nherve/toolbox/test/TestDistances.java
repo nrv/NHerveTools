830f0d2a450da848e90190195fed0fbc29fe3456
nrv <nherve@laposte.net>
2015-08-27 11:33:44 +0200

    junit

--- src/test/java/plugins/nherve/toolbox/test/TestDistances.java
+++ src/test/java/plugins/nherve/toolbox/test/TestDistances.java
@@ -2,6 +2,10 @@ package plugins.nherve.toolbox.test;
 
 import java.util.Random;
 
+import junit.framework.Assert;
+
+import org.junit.Test;
+
 import plugins.nherve.toolbox.image.feature.SignatureDistance;
 import plugins.nherve.toolbox.image.feature.signature.DenseVectorSignature;
 import plugins.nherve.toolbox.image.feature.signature.DynamicSparseVectorSignature;
@@ -14,14 +18,14 @@ import plugins.nherve.toolbox.image.feature.signature.VectorSignature;
 
 public class TestDistances {
 
-	public static Random rd = new Random(1586412l);
+	public Random rd = new Random(1586412l);
 
-	public static double[] randomData(int dim, double sparsity, boolean negValues) {
+	public double[] randomData(int dim, double sparsity, boolean negValues) {
 		double[] data = new double[dim];
 		for (int d = 0; d < dim; d++) {
 			if (rd.nextDouble() >= sparsity) {
 				if (negValues) {
-					data[d] = 1 - 2 * rd.nextDouble();
+					data[d] = 1 - (2 * rd.nextDouble());
 				} else {
 					data[d] = rd.nextDouble();
 				}
@@ -32,7 +36,24 @@ public class TestDistances {
 		return data;
 	}
 
-	public static void test(int dim, double sparsity, int nb, boolean negValues, boolean norm) throws SignatureException {
+	@Test
+	public void runAllTests() {
+		try {
+			for (int dim = 2; dim < 103; dim += 10) {
+				for (double sparsity = 0; sparsity < 0.9; sparsity += 0.2) {
+					test(dim, sparsity, 10, true, true);
+					test(dim, sparsity, 10, false, true);
+					test(dim, sparsity, 10, true, false);
+					test(dim, sparsity, 10, false, false);
+				}
+			}
+		} catch (SignatureException e) {
+			Assert.fail(e.getClass().getName() + " : " + e.getMessage());
+		}
+	}
+
+	public void test(int dim, double sparsity, int nb, boolean negValues, boolean norm) throws SignatureException {
+		System.out.println("test(" + dim + ", " + sparsity + ", " + nb + ", " + negValues + ", " + norm + ")");
 		final int sigType = 3;
 		final int distType = 3;
 
@@ -72,13 +93,13 @@ public class TestDistances {
 						for (int n = 0; n < sigType; n++) {
 							double computed = dist[d].computeDistance(sigs[m][i], sigs[n][j]);
 							if (idx > 0) {
-								if (computed != previous) {
-									System.err.println("Error (" + dist[d].getClass().getName() + ") : " + previous + " / " + computed);
-									System.err.println(" - sigs[" + m + "][" + i + "] : " + sigs[m][i]);
-									System.err.println(" - sigs[" + n + "][" + j + "] : " + sigs[n][j]);
-									System.err.println("");
-									System.exit(1);
-								}
+								Assert.assertEquals(previous, computed);
+								// if (computed != previous) {
+								// System.err.println("Error (" + dist[d].getClass().getName() + ") : " + previous + " / " + computed);
+								// System.err.println(" - sigs[" + m + "][" + i + "] : " + sigs[m][i]);
+								// System.err.println(" - sigs[" + n + "][" + j + "] : " + sigs[n][j]);
+								// System.err.println("");
+								// }
 							}
 							idx++;
 							previous = computed;
@@ -88,21 +109,4 @@ public class TestDistances {
 			}
 		}
 	}
-
-	public static void main(String[] args) {
-		try {
-			for (int dim = 2; dim < 100; dim++) {
-				for (double sparsity = 0; sparsity < 0.9; sparsity += 0.1) {
-					System.out.println("test(" + dim + ", " + sparsity + ", 10);");
-					test(dim, sparsity, 10, true, true);
-					test(dim, sparsity, 10, false, true);
-					test(dim, sparsity, 10, true, false);
-					test(dim, sparsity, 10, false, false);
-				}
-			}
-		} catch (SignatureException e) {
-			e.printStackTrace();
-		}
-	}
-
 }
